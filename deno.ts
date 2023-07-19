9360d509690f1a62220c35eb98c7dd7d0c964492
jaden <49010414+Mrxyy@users.noreply.github.com>
2023-07-19 23:26:49 +0800

    Update deno.ts

--- deno.ts
+++ deno.ts
@@ -1,14 +1,33 @@
-import { serve } from "https://deno.land/std@0.181.0/http/server.ts";
+import { serve } from "https://deno.land/std/http/server.ts";
 
 const OPENAI_API_HOST = "api.openai.com";
 
 serve(async (request) => {
-  const url = new URL(request.url);
+  const url = new URL(request.url, `http://${request.headers.get("host")}`);
 
   if (url.pathname === "/") {
-    return fetch(new URL("./Readme.md", import.meta.url));
+    const readmeUrl = new URL("./Readme.md", import.meta.url);
+    const readmeResponse = await fetch(readmeUrl);
+    return {
+      status: readmeResponse.status,
+      headers: readmeResponse.headers,
+      body: readmeResponse.body,
+    };
   }
 
   url.host = OPENAI_API_HOST;
-  return await fetch(url, request);
+  const targetUrl = url.toString();
+
+  const response = await fetch(targetUrl, {
+    method: request.method,
+    headers: request.headers,
+    body: request.body,
+  });
+
+  const responseHeaders = new Headers(response.headers);
+  return {
+    status: response.status,
+    headers: responseHeaders,
+    body: response.body,
+  };
 });
