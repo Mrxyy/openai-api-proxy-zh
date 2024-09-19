52d1ac35c8ea3a70354e95a75ce0b7213e13b371
Mrxyy <49010414+Mrxyy@users.noreply.github.com>
2024-09-19 15:00:02 +0800

    Update deno.ts

--- deno.ts
+++ deno.ts
@@ -1,6 +1,6 @@
 import { Application, proxy } from 'https://deno.land/x/oak/mod.ts';
 
 const app = new Application({proxy:true});
-app.use(proxy('https://api.openai.com'));
+app.use(proxy('https://generativelanguage.googleapis.com'));
 
 app.listen({ port: 8000 });
